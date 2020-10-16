98ad95aa5f4423b45cb1f50e82eba3074a128a81
ErickCristhian <30393905+ErickCristhian@users.noreply.github.com>
2020-10-16 10:04:37 -0300

    The Sims - Desafio das Fábricas

---
+++ TheSimsFabricas/src/model/VestimentaClassica.java
@@ -0,0 +1,12 @@
+package model;
+
+public class VestimentaClassica extends Vestimenta {
+
+	public VestimentaClassica() {
+		super();
+		setCamisa("Social Branca");
+		setAcessorio("Relógio");
+		setSapato("Sapato Social");
+	}
+
+}

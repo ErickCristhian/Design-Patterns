2f49bacd7203aa15dd4455e8f9e7badd37c2eb49
ErickCristhian <30393905+ErickCristhian@users.noreply.github.com>
2020-10-18 21:01:46 -0300

    Adapter

---
+++ Adapter/src/Adaptador.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Adaptador extends SomadorExistente implements SomadorEsperado {
+
+	public Adaptador() {}
+
+	@Override
+	public int somaVetor(int[] vetor) {
+		
+		ArrayList<Integer> listInt = IntStream.of(vetor).boxed().collect(Collectors.toCollection(ArrayList::new)); 				
+		int listSoma = somaLista((List<Integer>) listInt);		
+		return listSoma;
+	}
+
+	
+
+	 
+	
+
+}

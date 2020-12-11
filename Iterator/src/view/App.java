aafb2a0faa4f299c26166e5b06091c3d481ff156
ErickCristhian <30393905+ErickCristhian@users.noreply.github.com>
2020-12-11 19:58:15 -0300

    Add Iterator

--- Iterator/src/view/App.java
+++ Iterator/src/view/App.java
@@ -2,26 +2,26 @@ package view;
 
 import java.util.Iterator;
 
-import model.LiveRepository;
-import model.DropRepository;
+import model.EsporteRepository;
+import model.FilmeRepository;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		DropRepository dropRepository = new DropRepository();
-		LiveRepository liveRepository = new LiveRepository();
-		
-		for(Iterator<String> iterator = dropRepository.getIterator(); iterator.hasNext();) {
-			String drop = iterator.next();
-			System.out.println("drop: " + drop);
-		}	
-		
+		FilmeRepository filmeRepository = new FilmeRepository();
+		EsporteRepository esporteRepository = new EsporteRepository();
+	
+		for(Iterator<String> iterator = filmeRepository.getIterator(); iterator.hasNext();) {
+			String filme = iterator.next();
+			System.out.println("Filme: " + filme);
+		}
+			
 		System.out.println();
 		
-		for(Iterator<String> iterator = liveRepository.getIterator(); iterator.hasNext();) {
-			String live = iterator.next();
-			System.out.println("live: " + live);
+		for(Iterator<String> iterator = esporteRepository.getIterator(); iterator.hasNext();) {
+			String esporte = iterator.next();
+			System.out.println("Esporte: " + esporte);
 		}
 
 	}
